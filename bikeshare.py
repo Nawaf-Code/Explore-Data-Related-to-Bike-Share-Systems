d2d8226713b07ee3dc7206564f411d428bdb0708
Nawaf-Code <98234284+Nawaf-Code@users.noreply.github.com>
2022-12-17 22:56:46 +0300

    refactor: call function print_my_info() in main

--- bikeshare.py
+++ bikeshare.py
@@ -234,7 +234,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df,city)
         raw_data(df)
-
+        print_my_info()
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
